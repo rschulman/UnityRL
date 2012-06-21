22bac7e09db691632150397de058de6730bf8857
Ross Schulman <rschulman@gmail.com>
2012-06-21 18:05:56 -0400

    levels now have hubs

--- level.go
+++ level.go
@@ -2,141 +2,181 @@ package main
 
 import "math/rand"
 
-type Player struct {}
-type Mob struct {}
+type Player struct{} // stubs... to be moved to their own files later.
+type Mob struct{}
 
 type tile struct { // single tile
-    physical string // "floor" or "wall" or "upstair" etc
+	physical string // "floor" or "wall" or "upstair" etc
 
 }
 
 type point struct {
-    x int
-    y int
+	x int
+	y int
 }
 
 type room struct {
-    x uint
-    y uint
-    width uint
-    height uint
+	x      int
+	y      int
+	width  int
+	height int
 }
 
 type Level struct {
-    MAXROWS, MAXCOLS uint
-    data [][]tile
-    players map[string]Player
-    mobs map[int]Mob
-    depth uint
+	MAXROWS, MAXCOLS int
+	data             [][]tile
+	players          map[string]Player
+	mobs             map[int]Mob
+	depth            uint
+	h                hub
 }
 
 func roomValid(l *Level, proposal room) bool {
-    if proposal.x > 0 && proposal.y > 0 && proposal.y + proposal.height <= l.MAXROWS && proposal.x + proposal.width <= l.MAXCOLS {
-        for row := proposal.y; row < proposal.y + proposal.height; row++ {
-            for col := proposal.x; col < proposal.x + proposal.width; col++ {
-                if l.data[col][row].physical != " " {
-                    return false
-                }
-            }
-        }
-        return true
-    } else {
-        return false
-    }
+	if proposal.x > 0 && proposal.y > 0 && proposal.y+proposal.height <= l.MAXROWS && proposal.x+proposal.width <= l.MAXCOLS {
+		for row := proposal.y; row < proposal.y+proposal.height; row++ {
+			for col := proposal.x; col < proposal.x+proposal.width; col++ {
+				if l.data[col][row].physical != " " {
+					return false
+				}
+			}
+		}
+		return true
+	}
+	return false
 }
 
-func (l *Level) buildlevel () {
-    for x := 0; x <= int(l.MAXROWS); x++ {
-        for y := 0; y <= int(l.MAXCOLS); y++ {
-            l.data[x][y].physical = " "
-        }
-    }
-
-    roomx := uint(rand.Intn(10) + 5)
-    roomy := uint(rand.Intn(10) + 5)
-    roomw := uint(rand.Intn(10) + 5)
-    roomh := uint(rand.Intn(10) + 5)
-    digroom(roomx, roomy, roomw, roomh, l.data)
-    rooms := make([]room, 1)
-    rooms[0] = room{roomx, roomy, roomw, roomh}
-    for room_counter := 0; room_counter < 25; room_counter++ {
-        valid := false
-        for valid == false {
-            working_room := rand.Intn(len(rooms))
-            exitlength := rand.Intn(7) + 5
-            exitpoints := make([]point, 0)
-            switch rand.Intn(4) + 1 {
-            case 1: // North wall of the room
-                exitx := rand.Intn(int(rooms[working_room].height)) + rooms[working_room].x
-                exity := rooms[working_room].y
-                startx := exitx - rand.Intn(roomw)
-                starty := exity - roomh - exitlength - 1
-                for hallways := 0; hallways < exitlength; hallways ++ {
-                    exitpoints.append(point{exitx, exity + hallways})
-                }
-            case 2: // East wall of the room
-                exitx := rooms[working_room].x + rooms[working_room].width
-                exity := rand.Intn(int(rooms[working_room].height)) + rooms[working_room].y
-                startx := exitx + exitlength
-                starty := exity - rand.Intn(int(rooms[working_room].height))
-                for hallways := 0; hallways < exitlength; hallways ++ {
-                    exitpoints.append(point{exitx + hallways, exity})
-                }
-            case 3: // South wall of the room
-                exitx := rand.Intn(int(rooms[working_room].height)) + rooms[working_room].x
-                exity := rooms[working_room].y + rooms[working_room].height
-                startx := exitx - rand.Intn(roomw)
-                starty := exity + exitlength
-                for hallways := 0; hallways < exitlength; hallways ++ {
-                    exitpoints.append(point{exitx + hallways, exity})
-                }
-            case 4: // West wall of the room
-                exitx := rooms[working_room].x
-                exity := rand.Intn(int(rooms[working_room].height)) + rooms[working_room].y
-                startx := exitx - roomw - exitlength - 1
-                starty := exity - rand.Intn(int(roomh))
-                for hallways := 0; hallways < exitlength; hallways ++ {
-                    exitpoints.append(point{exitx + hallways, exity})
-                }
-            }
-
-            proposal := room{startx, starty, roomw, roomh}
-            valid = roomValid(l, proposal)
-        }
-        digroom(l, proposal)
-        rooms.append(proposal)
-    }
-    stairdone := false
-    for stairdone == false {
-        stairx := rand.Intr(l.MAXCOLS)
-        stairy := rand.Intr(l.MAXROWS)
-        if l.data[stairx][stairy] == "floor" {
-            l.data[stairx][stairy] = "upstair"
-            stairdone = true;
-        }
-    }
-    stairdone = false
-    for stairdone == false {
-        stairx := rand.Intr(l.MAXCOLS)
-        stairy := rand.Intr(l.MAXROWS)
-        if l.data[stairx][stairy] == "floor" {
-            l.data[stairx][stairy] = "downstair"
-            stairdone = true;
-        }
-    }
+func (l *Level) digroom(proposal room) {
+	for col := proposal.x - 1; col < proposal.x+proposal.width; col++ {
+		l.data[col][proposal.y-1].physical = "wall"
+		l.data[col][proposal.y+proposal.height].physical = "wall"
+	}
+	for row := proposal.y - 1; row < proposal.y+proposal.height; row++ {
+		l.data[proposal.x-1][row].physical = "wall"
+		l.data[proposal.x+proposal.width][row].physical = "wall"
+	}
+	for col := proposal.x; col < proposal.x+proposal.width-1; col++ {
+		for row := proposal.y; row < proposal.y+proposal.height-1; row++ {
+			l.data[col][row].physical = "floor"
+		}
+	}
 }
 
-func generate (dlvl uint) Level {
-    working := Level{MAXROWS:999, MAXCOLS:999}
-    working.data = make([][]tile, working.MAXROWS)
-    for i := range working.data {
-        working.data[i] = make([]tile, working.MAXCOLS)
-        for j := range working.data[i] {
-            working.data[i][j] = tile{" "}
-        }
-    }
-    working.players = make(map[string]Player)
-    working.mobs = make(map[int]Mob)
-    working.depth = dlvl
-    return working
+func (l *Level) digpoint(target point) {
+	for xmod := -1; xmod <= 1; xmod++ {
+		for ymod := -1; ymod <= 1; ymod++ {
+			if l.data[target.x+xmod][target.y+ymod].physical == " " {
+				l.data[target.x+xmod][target.y+ymod].physical = "wall"
+			}
+		}
+	}
+	l.data[target.x][target.y].physical = "floor"
+}
+
+func (l *Level) buildlevel() {
+	for x := 0; x <= int(l.MAXROWS); x++ {
+		for y := 0; y <= int(l.MAXCOLS); y++ {
+			l.data[x][y].physical = " "
+		}
+	}
+
+	roomx := rand.Intn(10) + 5
+	roomy := rand.Intn(10) + 5
+	roomw := rand.Intn(10) + 5
+	roomh := rand.Intn(10) + 5
+	firstroom := room{roomx, roomy, roomw, roomh}
+	l.digroom(firstroom)
+	rooms := make([]room, 1)
+	rooms[0] = room{roomx, roomy, roomw, roomh}
+	for room_counter := 0; room_counter < 25; room_counter++ {
+		valid := false
+		var proposal room
+		exitpoints := make([]point, 0)
+		for valid == false {
+			working_room := rand.Intn(len(rooms))
+			exitlength := rand.Intn(7) + 5
+			var startx, starty int
+			switch rand.Intn(4) + 1 {
+			case 1: // North wall of the room
+				exitx := rand.Intn(int(rooms[working_room].height)) + rooms[working_room].x
+				exity := rooms[working_room].y
+				startx = exitx - rand.Intn(roomw)
+				starty = exity - roomh - exitlength - 1
+				for hallways := 0; hallways < exitlength; hallways++ {
+					exitpoints = append(exitpoints, point{exitx, exity + hallways})
+				}
+			case 2: // East wall of the room
+				exitx := rooms[working_room].x + rooms[working_room].width
+				exity := rand.Intn(int(rooms[working_room].height)) + rooms[working_room].y
+				startx = exitx + exitlength
+				starty = exity - rand.Intn(int(rooms[working_room].height))
+				for hallways := 0; hallways < exitlength; hallways++ {
+					exitpoints = append(exitpoints, point{exitx + hallways, exity})
+				}
+			case 3: // South wall of the room
+				exitx := rand.Intn(int(rooms[working_room].height)) + rooms[working_room].x
+				exity := rooms[working_room].y + rooms[working_room].height
+				startx = exitx - rand.Intn(roomw)
+				starty = exity + exitlength
+				for hallways := 0; hallways < exitlength; hallways++ {
+					exitpoints = append(exitpoints, point{exitx + hallways, exity})
+				}
+			case 4: // West wall of the room
+				exitx := rooms[working_room].x
+				exity := rand.Intn(int(rooms[working_room].height)) + rooms[working_room].y
+				startx = exitx - roomw - exitlength - 1
+				starty = exity - rand.Intn(int(roomh))
+				for hallways := 0; hallways < exitlength; hallways++ {
+					exitpoints = append(exitpoints, point{exitx + hallways, exity})
+				}
+			}
+
+			proposal = room{startx, starty, roomw, roomh}
+			valid = roomValid(l, proposal)
+		}
+		l.digroom(proposal)
+		for iter := 0; iter < len(exitpoints); iter++ {
+			l.digpoint(exitpoints[iter])
+		}
+		rooms = append(rooms, proposal)
+	}
+	stairdone := false
+	for stairdone == false {
+		stairx := rand.Intn(l.MAXCOLS)
+		stairy := rand.Intn(l.MAXROWS)
+		if l.data[stairx][stairy].physical == "floor" {
+			l.data[stairx][stairy].physical = "upstair"
+			stairdone = true
+		}
+	}
+	stairdone = false
+	for stairdone == false {
+		stairx := rand.Intn(l.MAXCOLS)
+		stairy := rand.Intn(l.MAXROWS)
+		if l.data[stairx][stairy].physical == "floor" {
+			l.data[stairx][stairy].physical = "downstair"
+			stairdone = true
+		}
+	}
+}
+
+func generate(dlvl uint) Level {
+	working := Level{MAXROWS: 999, MAXCOLS: 999}
+	working.data = make([][]tile, working.MAXROWS)
+	for i := range working.data {
+		working.data[i] = make([]tile, working.MAXCOLS)
+		for j := range working.data[i] {
+			working.data[i][j] = tile{" "}
+		}
+	}
+	working.h = hub{
+		broadcast:   make(chan string),
+		register:    make(chan *connection),
+		unregister:  make(chan *connection),
+		connections: make(map[*connection]bool),
+	}
+	working.players = make(map[string]Player)
+	working.mobs = make(map[int]Mob)
+	working.depth = dlvl
+	working.h.run()
+	return working
 }
--- unity.go
+++ unity.go
@@ -3,30 +3,96 @@
 package main
 
 import (
-        "fmt"
-        "net/http"
-        "code.google.com/p/go.net/websocket"
-        "text/template"
+	"fmt"
+	"net/http"
+	"code.google.com/p/go.net/websocket"
+	"text/template"
 )
 
 type connection struct {
-    ws *websocket.Conn
+	ws *websocket.Conn
 
-    send chan string
+	send chan string
+}
+
+func (c *connection) reader() {
+	for {
+		var message string
+		err := websocket.Message.Receive(c.ws, &message)
+		if err != nil {
+			break
+		}
+		h.broadcast <- message
+	}
+	c.ws.Close()
+}
+
+func (c *connection) writer() {
+	for message := range c.send {
+		err := websocket.Message.Send(c.ws, message)
+		if err != nil {
+			break
+		}
+	}
+	c.ws.Close()
+}
+
+func wsHandler(ws *websocket.Conn) {
+	c := &connection{send: make(chan string, 256), ws: ws}
+	h.register <- c
+	defer func() { h.unregister <- c }()
+	go c.writer()
+	c.reader()
+}
+
+type hub struct {
+	// Registered connections.
+	connections map[*connection]bool
+
+	// Inbound messages from the connections.
+	broadcast chan string
+
+	// Register requests from the connections.
+	register chan *connection
+
+	// Unregister requests from connections.
+	unregister chan *connection
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case c := <-h.register:
+			h.connections[c] = true
+		case c := <-h.unregister:
+			delete(h.connections, c)
+			close(c.send)
+		case m := <-h.broadcast:
+			for c := range h.connections {
+				select {
+				case c.send <- m:
+				default:
+					delete(h.connections, c)
+					close(c.send)
+					go c.ws.Close()
+				}
+			}
+		}
+	}
 }
 
 var rootTempl = template.Must(template.ParseFiles("index.html"))
 
-func rootHandler (c http.ResponseWriter, req *http.Request) {
-    rootTempl.Execute(c, req.Host)
+func rootHandler(c http.ResponseWriter, req *http.Request) {
+	rootTempl.Execute(c, req.Host)
 }
 
 func main() {
-    http.HandleFunc("/", rootHandler)
-    if err := http.ListenAndServe("localhost:8080", nil); err != nil {
-        fmt.Println("error")
-    }
-    var level = generate(1)
-    level.buildlevel()
-
+	http.HandleFunc("/", rootHandler)
+	http.Handle("/ws", websocket.Handler(wsHandler))
+	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+		fmt.Println("error")
+	}
+	var groundfloor = generate(1)
+	groundfloor.buildlevel()
 }
